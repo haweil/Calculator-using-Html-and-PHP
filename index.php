8b2934df7838b8b0f80ffd9e6c223b667bd4e6eb
haweil <92003311+haweil@users.noreply.github.com>
2023-08-11 02:28:15 +0300

    Update Style

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
-<?php
-    declare (Strict_types=1);
-    include "project/autoload.inc.php";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Frist project</title>
-</head>
+    <title>First Project</title>
+    <link rel="stylesheet" href="style.css">
 
 <body>
-    <form action="project/calc.inc.php" method="post">
-        <P>My Own Calculator</P>
-        <input type="number" name="num1" placeholder="First Number">
-        <select name="oper">
-            <option value="add">Addition</option>
-            <option value="sub">Subtraction</option>
-            <option value="div">Division</option>
-            <option value="mul">Multiplication</option>
-            <option value="mod">Modulo</option>
-        </select>
-        <input type="number" name="num2" placeholder="Second Number">
-        <button type="submit" name="submit">Calculator</button>
-    </form>
-    <?php
-    ?>
+    <div class="container">
+        <h1>My Own Calculator</h1>
+        <form action="project/calc.inc.php" method="post">
+            <div class="form-group">
+                <label for="num1">First Number:</label>
+                <input type="number" name="num1" id="num1" placeholder="First Number" required>
+            </div>
+            <div class="form-group">
+                <label for="oper">Operation:</label>
+                <select name="oper" id="oper">
+                    <option value="add">Addition</option>
+                    <option value="sub">Subtraction</option>
+                    <option value="div">Division</option>
+                    <option value="mul">Multiplication</option>
+                    <option value="mod">Modulo</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="num2">Second Number:</label>
+                <input type="number" name="num2" id="num2" placeholder="Second Number" required>
+            </div>
+            <button type="submit" name="submit">Calculate</button>
+        </form>
+    </div>
 </body>
 
 </html>
